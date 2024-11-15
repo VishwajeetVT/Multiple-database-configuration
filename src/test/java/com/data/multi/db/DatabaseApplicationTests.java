c6bb3b4ed0123ac4f8e53cdabf0c10073eef5f55
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-11-15 17:12:54 +0530

    Add annotations

--- src/test/java/com/data/multi/db/DatabaseApplicationTests.java
+++ src/test/java/com/data/multi/db/DatabaseApplicationTests.java
@@ -3,7 +3,7 @@ package com.data.multi.db;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {DatabaseApplication.class, FlywayTestConfiguration.class})
 class DatabaseApplicationTests {
 
 	@Test
