93b2e5b3c9f3e23ade10a1dbe9bd4feb668870a3
VishwajeetK <vishwajeet.k@vervetronics.com>
2024-11-15 16:27:28 +0530

    Add migrate method in main class

--- src/main/java/com/data/multi/db/DatabaseApplication.java
+++ src/main/java/com/data/multi/db/DatabaseApplication.java
@@ -1,13 +1,27 @@
 package com.data.multi.db;
 
+import jakarta.annotation.PostConstruct;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DatabaseApplication {
 
+	@Autowired
+	private Flyway userFlyway;
+
+	@Autowired
+	private Flyway productFlyway;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
 	}
 
+	@PostConstruct
+	public void migrate() {
+		userFlyway.migrate();
+		productFlyway.migrate();
+	}
 }
